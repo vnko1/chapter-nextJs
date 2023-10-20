39a89c8e334ff20c161448d95de86280b51a98ac
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-10-20 07:52:02 +0300

    feat: base config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.scss";
+import "/src/styles/index.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
